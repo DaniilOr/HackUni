e280e096ad81157ef3b1c5fca0270e45f8948461
Daniil <36066254+DaniilOr@users.noreply.github.com>
2020-03-22 18:21:45 +0600

    Добавлен модуль для отпраки сообщений по почте

--- useful_tools.py
+++ useful_tools.py
@@ -1,5 +1,14 @@
 import re
-from models import db, User
+from app.models import db, User
+import smtplib
+import ssl
+import json
+
+class PersonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, User):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
 
 
 def delete_numbers(string):
@@ -11,7 +20,21 @@ def delete_emails(string):
     return result
 
 def uname_check(uname):
-    return db.query(User).filter(User.username == uname).first()
+    return db.session.query(User).filter(User.username == uname).first()
 
 def email_check(email):
-    return db.query(User).filter(User.email == email).first()
+    return db.session.query(User).filter(User.email == email).first()
+
+def send_mail(email, description='День рождения сына!'):
+    sender_email = "notificationssender.eHack@gmail.com"
+    password = "##CENSURED##"
+    port = 465  # For SSL
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        message = '''Subject: Вас выбрали для меропртиятия!\n\nДоброго времени суток, Вы были выбраны для меропртиятия. Вот его описание:
+{}
+        '''.format(description)
+        message = message.encode('utf-8')
+        server.login(sender_email, password)
+        server.sendmail(sender_email, email, message)
